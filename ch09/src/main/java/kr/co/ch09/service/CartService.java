b6a64c409e74e21a835584850d37d77adfe6a606
mincheolkang34 <mincheolkang34@gmail.com>
2025-11-04 09:35:59 +0900

    flutter Kmarket 연계 기능 구현

---
+++ ch09/src/main/java/kr/co/ch09/service/CartService.java
@@ -0,0 +1,31 @@
+package kr.co.ch09.service;
+
+import kr.co.ch09.dto.CartDTO;
+import kr.co.ch09.entity.Cart;
+import kr.co.ch09.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CartService {
+    private final CartRepository cartRepository;
+
+    public CartDTO save(CartDTO cartDTO) {
+        Cart cart = cartDTO.toEntity();
+        Cart savedCart = cartRepository.save(cart);
+        return savedCart.toDTO();
+    }
+
+    public List<CartDTO> findByUserid(String userid) {
+        List<Cart> cartList = cartRepository.findByUserid(userid);
+        return cartList
+                .stream()
+                .map(Cart::toDTO)
+                .toList();
+    }
+}
